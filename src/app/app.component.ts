9341de162bdf836aa66f5f1acc2a827b7dace539
ashar708 <aarya_sharma@optum.com>
2024-05-27 19:23:01 +0530

    Added ng form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,CommonModule],
+  imports: [RouterOutlet,FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -22,4 +23,24 @@ export class AppComponent {
     {name:"Alice",Proffession:"Doctor",Age:"35"},
     {name:"Andrew",Proffession:"Lawyer",Age:"20"}
   ]
+
+  regForm: FormGroup;
+  constructor(){
+
+  }
+  ngOnInit(){
+    this.regForm = new FormGroup({
+      fname: new FormControl(),
+      lname: new FormControl(),
+      office: new FormControl(),
+      username: new FormControl(),
+      password: new FormControl(),
+      confirm_password: new FormControl(),
+      email: new FormControl(),
+      contactno: new FormControl(),
+    })
+  }
+  register(){
+    console.log(this.regForm.value);
+  }
 }
